1a292523b12e49fa8f7983aa70f2623e1ab23b03
Willy Aguirre <marti1125@gmail.com>
2013-12-03 08:39:18 -0500

    update ref

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-header("Location: http://127.0.0.1/makiavelo/doc/doc.php");
+require('Parsedown.php');
+
+$doc = file_get_contents("README.markdown");
+
+$result = Parsedown::instance()->parse($doc);
+echo $result;
 
 ?>
