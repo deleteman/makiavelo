fb980a3135dc9cb049eda87e2ce73ae1757f3f8d
Dickriven Chellemboyee <cd@esokia-webagency.com>
2015-07-28 16:32:03 +0400

    update ActionFactory
    - make singleton
    - put all properties at the top and methods at the bottom
    - throw exception when action code is not in mapping
    - apply cs

--- core/ActionFactoryClass.php
+++ core/ActionFactoryClass.php
@@ -1,28 +1,41 @@
 <?php
 
-class ActionFactory {
+class ActionFactory
+{
+    private static $instance = null;
 
-	private static $instance = null;
+    private $mapping = array(
+        'g' => 'GeneratorAction',
+        'db:load' => 'DBLoaderAction',
+        'db:create' => 'DBCreatorAction',
+        'db:migrate' => 'DBMigratorAction',
+        'db:rollback' => 'DBMigratorRollbackAction',
+        'task' => 'TaskRunnerAction',
+    );
 
-	public static function getInstance() {
-		if(self::$instance == null) {
-			self::$instance = new ActionFactory();
-		}
-		return self::$instance;
-	}
+    private function __construct()
+    {
+    }
 
-	private $mapping = array(
-		"g" => "GeneratorAction",
-		"db:load" => "DBLoaderAction",
-		"db:create" => "DBCreatorAction",
-		"db:migrate" => "DBMigratorAction",
-		"db:rollback" => "DBMigratorRollbackAction",
-		"task" => "TaskRunnerAction"
-		);
+    private function __clone()
+    {
+    }
 
-	public function getAction($action_code) {
-		return new $this->mapping[$action_code];
-	}
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new ActionFactory();
+        }
+
+        return self::$instance;
+    }
+
+    public function getAction($action_code)
+    {
+        if (isset($this->mapping[$action_code])) {
+            return new $this->mapping[$action_code]();
+        }
+
+        throw new Exception("Invalid action - {$action_code}");
+    }
 }
-
-?>
